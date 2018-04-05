0cb797c8ec88930e086bc9fefcec7fe0f0c08596
Hazem Gharib <hazem.gharib.8@gmail.com>
2018-04-05 19:16:58 +0200

    Adding submit/reset buttons alongside other UI fixes

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -11,6 +12,26 @@ export class ProductComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  onSubmit(productForm: NgForm) {
+    console.log('Hello');
+  }
+
+  resetForm(productForm?: NgForm) {
+    console.log('reset');
+    if (productForm != null) {
+      productForm.reset();
+    }
+
+    this.productService.selectedProduct = {
+      $key: null,
+      Name: '',
+      Description: '',
+      Count: 0,
+      Price: 0,
+      Category: ''
+    };
+  }
 }
