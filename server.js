0c1557f07e9ffc36a141bb8a06d64ff817408a3a
Hazem Gharib <hazem.gharib.8@gmail.com>
2018-04-11 16:09:55 +0200

    Adding fallback to the express server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const history = require('connect-history-api-fallback');
 const express = require('express');
 const app = express();
 
+app.use(history({
+    verbose: true
+  }));
+  
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
